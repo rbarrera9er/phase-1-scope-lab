aa3488ac91b7ea9c20189947c925f0cff007640e
Ryan Barrera <rbarrera9er@gmail.com>
2022-05-22 17:31:56 -0400

    commit tree 52720ef1756fbc8e92e91cafb0edae86db2e8daa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = ''
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = ''
+}
